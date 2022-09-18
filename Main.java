cb2356390e85d430519221520fff4b69b5f2b0b1
Alva <alva.storm@awacademy.training>
2022-09-18 13:56:06 +0200

    add text

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package enhörningInheritance;
 public class Main {
     public static void main(String[] args) {
     System.out.println("This is my inheritance practis: ");
+    System.out.println("As you can tell, it´s not done! ");
 
     }
 }
